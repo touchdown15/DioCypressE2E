129d67236349af1fcdbf59431c5c03b466d6fcc9
touchdown15 <chvs1524@gmail.com>
2021-02-08 12:42:16 -0300

    Commit Inicial

---
+++ cypress/integration/comentarios.spec.js
@@ -0,0 +1,12 @@
+const { createYield } = require("typescript")
+
+describe('Comentários', ()=>{
+  it('Escrever', ()=>{
+    cy.login('testecypress@testecypress.com', 'testecypress')
+    cy.contains('Global Feed').click()
+    cy.get('.preview-link').first().click()
+    cy.get('.form-control').type('Sensacional!')
+    cy.get('.btn-primary').click()
+    cy.contains('Sensacional!')
+  })
+})
---
+++ cypress/integration/logout.spec.js
@@ -0,0 +1,10 @@
+const { createYield } = require("typescript")
+
+describe('Logout', ()=>{
+  it('Logout via Perfil', ()=>{
+    cy.login('testecypress@testecypress.com', 'testecypress')
+    cy.contains('Settings').click()
+    cy.url().should('include', '/settings')
+    cy.get('.btn-outline-danger').click()
+  })
+})
